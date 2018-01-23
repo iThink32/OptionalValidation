b2711b8b0d1d3cd3eeb4fc42f9d417b90ed2067d
Shashank <34152717+iThink32@users.noreply.github.com>
2018-01-23 12:21:19 +0530

    Add files via upload

---
+++ OptionalValidation.swift
@@ -0,0 +1,35 @@
+//
+//  OptionalValidation.swift
+//  Rynly.Driver.iOS
+//
+//  Created by N A Shashank on 1/23/18.
+//  Copyright © 2018 N A Shashank. All rights reserved.
+//
+
+import UIKit
+
+protocol OptionalValidation {
+    func validate() -> ValidationError?
+}
+
+extension OptionalValidation {
+    
+    func validate() -> ValidationError? {
+        // first fetch all instance values for this class
+        let mirror = Mirror(reflecting: self)
+        for child in mirror.children
+        {
+            let subMirror = Mirror(reflecting:child.value)
+            guard subMirror.displayStyle == Mirror.DisplayStyle.optional else{
+                continue
+            }
+            guard subMirror.children.count == 0 else{
+                continue
+            }
+            return ValidationError(description: "Please fill all the required fields.")
+        }
+        return nil
+    }
+    
+}
+
