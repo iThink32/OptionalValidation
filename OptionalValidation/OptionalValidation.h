e7d1847f159e7a8a520e6ed38fe0d85d29612a3a
N Shashank <shashank@ns-macbook-pro.localdomain>
2018-03-01 11:20:58 +0530

    Initial Commit

---
+++ OptionalValidation/OptionalValidation.h
@@ -0,0 +1,19 @@
+//
+//  OptionalValidation.h
+//  OptionalValidation
+//
+//  Created by N A Shashank on 01/03/18.
+//  Copyright © 2018 N A Shashank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OptionalValidation.
+FOUNDATION_EXPORT double OptionalValidationVersionNumber;
+
+//! Project version string for OptionalValidation.
+FOUNDATION_EXPORT const unsigned char OptionalValidationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OptionalValidation/PublicHeader.h>
+
+
