3f74e0d76336d8ade3aaf3aaa3a3e53343a229a5
Shashank <34152717+iThink32@users.noreply.github.com>
2019-04-05 13:43:33 +0530

    Update OptionalValidation.swift

--- OptionalValidation.swift
+++ OptionalValidation.swift
@@ -34,5 +34,29 @@ extension OptionalValidation {
         return nil
     }
     
+    // recursive model check uncomment if required
+//         func validate(object:Any) -> ValidationError? {
+//         let mirror = Mirror(reflecting: object)
+//         for child in mirror.children {
+//             let subMirror = Mirror(reflecting: child.value)
+//             guard subMirror.children.count == 0 else{
+//                 guard let _ = validate(object: child.value) else {
+//                     continue
+//                 }
+//                 return ValidationError(description: "Please fill all the required fields.")
+//             }
+//             guard subMirror.displayStyle == Mirror.DisplayStyle.optional else{
+//                 //arrHolder.append(child.value)
+//                 continue
+//             }
+//             guard subMirror.children.count == 0 else{
+//                 //arrHolder.append(child.value)
+//                 continue
+//             }
+//             return ValidationError(description: "Please fill all the required fields.")
+//         }
+//         return nil
+//     }
+    
 }
 
